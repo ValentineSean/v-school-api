6e89926b6d92b634ff3f5407585a8bb4bf5e0480
Valentine Chanengeta <seanchanengeta@gmail.com>
2022-04-10 21:04:06 +0200

    saving shared links in db

--- controllers/topic/__init__.py
+++ controllers/topic/__init__.py
@@ -4,5 +4,6 @@ topic_blueprint = Blueprint("topic_blueprint", __name__)
 
 from . import create_topic
 from . import retrieve_topics
+from . import retrieve_subject_topics
 # from . import update_topic
 # from . import delete_topic
---
+++ controllers/topic/retrieve_subject_topics.py
@@ -0,0 +1,38 @@
+import json
+
+from flask import jsonify
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+
+from . import topic_blueprint
+from ...configurations.database import mongo
+
+@topic_blueprint.route("/retrieve-subject-topics/<subject_id>", methods=["GET"])
+def retrieve_subject_topics(subject_id):
+    subject_topics = mongo.db.topic.find(
+        {"$and":[
+            {
+                "subject_id": subject_id
+            },
+            {
+                "record_status": "ACTIVE",
+            }
+        ]}
+    )
+
+    if subject_topics:
+        subject_topics = json.loads(dumps(subject_topics))
+        subject_topics.reverse()
+
+        return jsonify({
+            "status": "200",
+            "message": "subject_topics_retrieved_ok",
+            "data": subject_topics
+        })
+
+    else:
+        return jsonify({
+            "status": "404",
+            "message": "subject_topics_not_found",
+            "data": []
+        })
