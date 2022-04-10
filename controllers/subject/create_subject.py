21cc1594f1c0201b75435e7a0dd441495008aa8c
Valentine Chanengeta <seanchanengeta@gmail.com>
2022-04-10 15:51:48 +0200

    database connected & create subject endpoint implemented

--- controllers/subject/create_subject.py
+++ controllers/subject/create_subject.py
@@ -1,9 +1,47 @@
-from flask import jsonify
+import json
+
+from flask import jsonify, request
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from datetime import datetime
 
 from . import subject_blueprint
+from ...configurations.database import mongo
 
 @subject_blueprint.route("/create-subject", methods=["POST"])
 def create_subject():
-    return jsonify({
-        "message": "create subject"
+    subject = request.json
+
+    subject_name = subject["subject_name"]
+    creation_date = datetime.now()
+    creation_date = creation_date.strftime("%Y-%m-%d %H:%M:%S")
+
+    new_subject_id = mongo.db.subject.insert_one({
+        "subject_name": subject_name,
+        "creation_date": creation_date,
+        "record_status": "ACTIVE"
+    }).inserted_id
+
+    new_subject = mongo.db.subject.find_one({
+        "$and": [
+            {"_id": ObjectId(new_subject_id)},
+            {"record_status": "ACTIVE"}
+        ]
     })
+
+    if new_subject:
+        new_subject = json.loads(dumps(new_subject))
+
+        return jsonify({
+            "status": "200",
+            "message": "subject_created_ok",
+            "data": new_subject
+        })
+
+    else:
+
+        return jsonify({
+            "status": "404",
+            "message": "subject_created_not_found",
+            "data": []
+        })
--- main/__init__.py
+++ main/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 # Configurations
+from ..configurations.database import mongo
+from ..configurations.database import database_credentials
 
 # Blueprints
 from ..controllers.subject_material import subject_material_blueprint
@@ -10,6 +12,13 @@ from ..controllers.subject import subject_blueprint
 app = Flask(__name__)
 
 CORS(app)
+
+app.config["MONGO_URI"] = "mongodb://{}/{}".format(
+    database_credentials["host"],
+    database_credentials["database"]
+)
+
+mongo.init_app(app)
 
 def create_app():
     app.register_blueprint(subject_material_blueprint)
